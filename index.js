a7fa903a4b73f53190cf0090ac39ea5fc3553470
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-03-25 15:09:40 +0530

    test

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ const port = 3010;
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+const revanth = require("./schema");
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+  return res.send({message:"This is  backend"});
+})
+
+const router = require("./router");
+
+app.use("/Sahithi",router);
+
+
+
+
+
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Server connected successfully")
+
+    console.log(`Example app listening at http://localhost:${port}`);
+  } catch (error) {
+    console.log("Error",error);
+  }
 });
